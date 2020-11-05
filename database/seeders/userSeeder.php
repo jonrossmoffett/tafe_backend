e56d95208ae20671d256c784b26309c778e553c3
BuildTools <unconfigured@null.spigotmc.org>
2020-11-05 21:08:20 +1000

    updated seeders

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Post;
 use illuminate\Database\Console\Factories;
 use Faker\Factory;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -19,6 +21,6 @@ class userSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(30)->create();
+        User::factory()->count(30)->has(Post::factory()->count(5)->state(new Sequence(['Status' => 0],['Status' => 1])))->create();
     }
 }
