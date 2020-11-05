ff41f980812ae2044adf56da577501ccba8cf92e
BuildTools <unconfigured@null.spigotmc.org>
2020-11-05 23:11:33 +1000

    edited search

--- resources/views/livewire/user-datatable.blade.php
+++ resources/views/livewire/user-datatable.blade.php
@@ -1,6 +1,6 @@
-<div>
+<div class="flex row">
 
-<div class="w-100">
+<div class="w--1/2">
 <div class="pt-2 relative mx-auto text-gray-600">
         <input wire:model="searchTerm" class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
           type="search" name="search" placeholder="Search">
