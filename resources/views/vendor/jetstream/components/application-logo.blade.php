8c518245fd4dc01487fe00b8d6aa37927b21c053
BuildTools <unconfigured@null.spigotmc.org>
2020-11-05 22:09:08 +1000

    edited welcome

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -4,7 +4,7 @@
   <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
 </svg> -->
 
-<div>
+<div class="w-25">
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 483 327" enable-background="new 0 0 483 327" xml:space="preserve" height="100%" width="100%">
 <g>
--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,3 +1,4 @@
+<div class="w-25">
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 483 327" enable-background="new 0 0 483 327" xml:space="preserve" height="100%" width="100%">
 <g>
@@ -10,3 +11,6 @@
 <polygon fill="#F26522" points="475.8,225.8 390.3,308.4 390.3,289.6 451.3,225.8 "/>
 <polygon fill="#F26522" points="374.7,234.6 400.1,238.4 398.7,212.7 474.4,295.7 475.8,321.4 450.3,317.7 "/>
 </svg>
+
+
+</div>
