7c74854b2d941b4aaef22394dd172f933b5f9b4c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-06 12:34:39 +1000

    edited search

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,4 +1,4 @@
-<div class="w-25">
+
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 483 327" enable-background="new 0 0 483 327" xml:space="preserve" {{ $attributes }}>
 <g>
@@ -13,4 +13,3 @@
 </svg>
 
 
-</div>
