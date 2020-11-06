1aa8bd59b955c16822fd11765d1440beb0d7970b
BuildTools <unconfigured@null.spigotmc.org>
2020-11-06 13:40:01 +1000

    edited search

--- app/Http/Livewire/PostsDatatable.php
+++ app/Http/Livewire/PostsDatatable.php
@@ -81,6 +81,7 @@ class PostsDatatable extends Component
         $this->editTitle = $post->Title;
         $this->editDescription = $post->Description;
         $this->editStatus = $post->Status;
+        dd($this->editStatus);
     }
 
     public function checkRole($id){
