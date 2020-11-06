e57416ecb62a4e781a9caa81ff5731216db70783
BuildTools <unconfigured@null.spigotmc.org>
2020-11-06 12:17:47 +1000

    toggle table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Aquarama Backend</title>
-        <link rel="shortcut icon" sizes="114x114" href="{{ asset('./images/favicon.svg') }}">
+        <link rel="shortcut icon" sizes="114x114" href="{{ asset('resources\images\favicon.svg') }}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
