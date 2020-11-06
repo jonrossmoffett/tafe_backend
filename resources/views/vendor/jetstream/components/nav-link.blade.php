97a3545e848e2503d02494662b2b5c2ef27e431d
BuildTools <unconfigured@null.spigotmc.org>
2020-11-06 11:52:49 +1000

    toggle table

--- resources/views/vendor/jetstream/components/nav-link.blade.php
+++ resources/views/vendor/jetstream/components/nav-link.blade.php
@@ -10,7 +10,7 @@ $classes = ($active ?? false)
     {{ $slot }}
 </a>
 
-@if(Auth::user()->hasRole('superadministrator','administrator'))
+@if(Auth::user()->hasRole('superadministrator') or Auth::user()->hasRole('administrator'))
 
 
 <a class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out" href="/admin">
